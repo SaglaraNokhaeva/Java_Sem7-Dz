6adc6bb2a54ff0b8448ffb8d464af2d76aed7aaa
Saglara <nohaevass@yandex.ru>
2023-01-30 18:28:38 +0300

    Update task1.java

--- task1.java
+++ task1.java
@@ -1,5 +1,5 @@
+// Пусть дан произвольный список целых чисел, удалить из него четные числа
 
-// Заполнить список десятью случайными числами. Отсортировать список методом sort() и вывести его на экран.
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Random;
@@ -8,14 +8,15 @@ public class task1 {
     public static void main(String[] args) {
         Random random = new Random();
         ArrayList<Integer> list1 = new ArrayList<>();
-        // int a = random.nextInt(10);
         for (int i = 0; i < 10; i++) {
-            list1.add(random.nextInt(10));
+            list1.add(random.nextInt(10));           
         }
-        list1.sort(Comparator.naturalOrder());
-        for (Object x : list1) {
-            System.out.print(x + " ");
+        System.out.println(list1);
+        for (int i = 0; i < list1.size(); i++) {
+            if (list1.get(i) % 2 == 0) {
+                list1.remove(i);
+            }
         }
+        System.out.println(list1);
     }
-
 }
